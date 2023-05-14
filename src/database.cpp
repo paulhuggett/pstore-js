929bbd35c6105539786e1af72359311a367171a2
Paul Bowen-Huggett <paulhuggett@mac.com>
2023-05-14 17:00:08 +0200

    First update for a long time.

--- src/database.cpp
+++ src/database.cpp
@@ -1,45 +1,45 @@
+
 #include "database.hpp"
 
-database::database (Napi::CallbackInfo const & info)
-        : ObjectWrap (info) {
-    Napi::Env env = info.Env ();
-    try {
-        if (info.Length () != 1) {
-            throw Napi::TypeError::New (env, "wrong number of arguments");
-        }
-
-        if (!info[0].IsString ()) {
-            Napi::TypeError::New (env, "Argument must be string (path to the database)")
-                .ThrowAsJavaScriptException ();
-            return;
-        }
-
-        auto const path = info[0].As<Napi::String> ().Utf8Value ();
-        db_ = std::make_shared<pstore::database> (path, pstore::database::access_mode::read_only);
-    } catch (std::exception const & ex) {
-        Napi::Error::New (env, ex.what ()).ThrowAsJavaScriptException ();
-    } catch (...) {
-        Napi::Error::New (env, "unknown error").ThrowAsJavaScriptException ();
-    }
-}
+#include <cfloat>
+#include <cmath>
+#include <optional>
+#include <iostream>
 
 namespace {
 
-    template <typename Function, typename... Args>
-    Napi::Value error_wrap (Napi::Env env, Function function, Args &&... args) {
-        try {
-            return function (std::forward<Args> (args)...);
-        } catch (Napi::Error const & err) {
-            throw;
-        } catch (std::exception const & ex) {
-            Napi::Error::New (env, ex.what ()).ThrowAsJavaScriptException ();
-        } catch (...) {
-            Napi::Error::New (env, "unknown error").ThrowAsJavaScriptException ();
-        }
-        return env.Null ();
+  template <typename Function, typename... Args>
+  decltype (auto) error_wrap (Napi::Env env, Function function, Args &&... args) {
+    try {
+      return function (std::forward<Args> (args)...);
+    } catch (Napi::Error const & err) {
+      throw;
+    } catch (std::exception const & ex) {
+      throw Napi::Error::New (env, ex.what ());
+    } catch (...) {
+      throw Napi::Error::New (env, "unknown error");
     }
+  }
 
 } // end anonymous namespace
+
+database::database (Napi::CallbackInfo const & info)
+        : ObjectWrap (info) {
+  Napi::Env env = info.Env ();
+  error_wrap (env, [&] () {
+    auto const path = info[0].As<Napi::String> ().Utf8Value ();
+    db_ = std::make_shared<pstore::database> (path, pstore::database::access_mode::read_only);
+
+    {
+      Napi::HandleScope scope (env);
+
+      Napi::Function ctor = myc::get_class (env);
+      constructor = Napi::Persistent (ctor);
+      constructor.SuppressDestruct ();
+      target.Set ("Canvas", ctor);
+    }
+  });
+}
 
 Napi::Value database::size (Napi::CallbackInfo const & info) {
     Napi::Env env = info.Env ();
@@ -75,20 +75,71 @@ Napi::Value database::revision (Napi::CallbackInfo const & info) {
                        [&] () { return Napi::Number::New (env, db_->get_current_revision ()); });
 }
 
+namespace {
+
+    std::int64_t as_int64 (Napi::Env env, Napi::Value const & value) {
+        auto const d = value.As<Napi::Number> ().DoubleValue ();
+        if (std::isnan (d) || std::isinf (d) || d < std::numeric_limits<std::int64_t>::min () ||
+            d > std::numeric_limits<std::int64_t>::max ()) {
+          throw Napi::TypeError::New (env, "An integer was expected");
+        }
+        return value.As<Napi::Number> ().Int64Value ();
+    }
+
+    void check_number_of_arguments (Napi::CallbackInfo const & info, unsigned const expected) {
+        if (info.Length () != expected) {
+          throw Napi::TypeError::New (info.Env (), "wrong number of arguments");
+        }
+    }
+
+} // end anonymous namespace
+
+Napi::Value database::sync (Napi::CallbackInfo const & info) {
+    Napi::Env env = info.Env ();
+
+
+    return error_wrap (env, [&] () {
+      check_number_of_arguments (info, 1U);
+      auto const v = as_int64 (env, info[0]);
+      auto revision = 0U;
+      if (v == head_revision) {
+        revision = pstore::head_revision;
+      } else if (v < std::numeric_limits<decltype (revision)>::min () ||
+                 v > std::numeric_limits<decltype (revision)>::max ()) {
+        throw Napi::TypeError::New (env, "Revision number out of range");
+      } else {
+        revision = static_cast<unsigned> (v);
+      }
+
+      db_->sync (revision);
+      return Napi::Number::New (env, db_->get_current_revision ());
+    });
+}
+
+Napi::Value database::get_index (Napi::CallbackInfo const & info) {
+    Napi::Env env = info.Env ();
+    check_number_of_arguments (info, 1U);
+    std::string const name = info[0].As<Napi::String> ().Utf8Value ();
+    if (name == "compilation") {
+    }
+    throw Napi::TypeError::New (env, "Unknown index");
+}
+
 Napi::Function database::get_class (Napi::Env env) {
     return DefineClass (env, "Database",
                         {
-                            database::InstanceMethod ("id", &database::id),
-                            database::InstanceMethod ("path", &database::path),
-                            database::InstanceMethod ("revision", &database::revision),
-                            database::InstanceMethod ("size", &database::size),
+                          database::InstanceMethod ("get_index", &database::get_index),
+                          database::InstanceMethod ("id", &database::id),
+                          database::InstanceMethod ("path", &database::path),
+                          database::InstanceMethod ("revision", &database::revision),
+                          database::InstanceMethod ("size", &database::size),
+                          database::InstanceMethod ("sync", &database::sync),
                         });
 }
 
 
 Napi::Object init (Napi::Env env, Napi::Object exports) {
-    Napi::String name = Napi::String::New (env, "Database");
-    exports.Set (name, database::get_class (env));
+    exports.Set (Napi::String::New (env, "Database"), database::get_class (env));
     return exports;
 }
 
--- src/database.hpp
+++ src/database.hpp
@@ -1,23 +1,61 @@
 #ifndef DATABASE_HPP
 #define DATABASE_HPP
 
+#include <cstdint>
+
 #include <napi.h>
 #include <pstore/core/database.hpp>
+#include <pstore/core/index_types.hpp>
 
-class database : public Napi::ObjectWrap<database> {
+class myc final : public Napi::ObjectWrap<myc> {
 public:
-    database (Napi::CallbackInfo const & info);
+  explicit myc (Napi::CallbackInfo const & info)
+          : ObjectWrap (info) {}
+  Napi::Value get (Napi::CallbackInfo const & info) {
+    Napi::Env env = info.Env ();
+    if (info.Length () != 0) {
+      throw Napi::TypeError::New (env, "wrong number of arguments");
+    }
+    return Napi::Number::New (env, 1234);
+  }
 
-    Napi::Value id (Napi::CallbackInfo const & info);
-    Napi::Value path (Napi::CallbackInfo const & info);
-    Napi::Value size (Napi::CallbackInfo const & info);
-    // Returns the revision to which the database is currently synched.
-    Napi::Value revision (Napi::CallbackInfo const & info);
+  static Napi::Function get_class (Napi::Env env) {
+    return DefineClass (env, "myc",
+                        {
+                          myc::InstanceMethod ("get", &myc::get),
+                        });
+  }
+};
 
-    static Napi::Function get_class (Napi::Env env);
+class database final : public Napi::ObjectWrap<database> {
+public:
+  explicit database (Napi::CallbackInfo const & info);
+
+  Napi::Value id (Napi::CallbackInfo const & info);
+  Napi::Value path (Napi::CallbackInfo const & info);
+  Napi::Value size (Napi::CallbackInfo const & info);
+
+  // Returns the revision to which the database is currently synched.
+  Napi::Value revision (Napi::CallbackInfo const & info);
+
+  static constexpr auto head_revision = std::int64_t{-1};
+
+  Napi::Value sync (Napi::CallbackInfo const & info);
+
+  Napi::Value get_index (Napi::CallbackInfo const & info);
+
+
+  static Napi::Function get_class (Napi::Env env);
 
 private:
-    std::shared_ptr<pstore::database> db_;
+  static Napi::FunctionReference constructor;
+  std::shared_ptr<pstore::database> db_;
+};
+
+class compilation_index : public Napi::ObjectWrap<compilation_index> {
+public:
+private:
+  std::shared_ptr<pstore::index::compilation_index> index_;
 };
 
 #endif // DATABASE_HPP
